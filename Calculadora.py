5659c7dfdb8f6febd1c283815b64c5795b6c5185
FFlores0914 <fflores0914@outlook.com>
2023-08-23 17:24:10 -0600

    Se agregaron las lineas de las 4 operaciones

--- Calculadora.py
+++ Calculadora.py
@@ -14,13 +14,24 @@ while True:
     print ("1. Suma.")
     print ("2. resta.")
     print ("3. Multiplicacione.")
-    print ("4. Division.")
-    print ("5. Salir.")
-
-    opcion = input ("Seleccione una Opcion.")
+    print ("4. Division")
+    print ("5.Salir")
+    opcion = input ("Seleccione una Opcion. ")
 
     if opcion == "5":
-        print ("Hasta Luego.")
+        print ("Hasta Luego")
         break
 
+    num1=float(input("Ingrese un numero. "))
+    num2=float(input("Ingrese un numero. "))
 
+    if opcion=="1":
+        print ("Resultado: ", suma(num1, num2))
+    elif opcion=="2":
+        print ("Resultado: ", resta(num1, num2))
+    elif opcion=="3":
+        print ("Resultado: ", multiplicacion(num1, num2))
+    elif opcion=="4":
+        print ("Resultado: ", division(num1, num2))
+    else:
+        print("Opcion Invalida.")
