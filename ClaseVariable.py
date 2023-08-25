f6aa375cd4729649ed56e12e9ca9e25ff3b525ef
FFlores0914 <fflores0914@outlook.com>
2023-08-24 19:13:46 -0600

    Se agrega nueva clase

---
+++ ClaseVariable.py
@@ -0,0 +1,7 @@
+nombre = input()
+apellido = input()
+edad = int(input())
+estatura = float(input())
+estudiante = bool(input())
+
+print(f"Hola {nombre} {apellido}, tu edad es {edad} años, mi estatura es de {estatura} cm, soy un estudiante: {estudiante}")
